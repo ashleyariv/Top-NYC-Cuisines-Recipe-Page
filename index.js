de8bb773cdca0f23efe19502c2baf52f4d8f9a59
ashleyariv <138259878+ashleyariv@users.noreply.github.com>
2023-08-14 14:53:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 fetch('http://localhost:3000/recipes')
 .then(response => response.json())
-.then(recipes => recipes.forEach(recipe => displayDetails(recipe)))
+.then(recipes => recipes.forEach(recipe => addCuisineToDivBar(recipe)))
 
 const recipeName = document.querySelector('#recipe-name')
 const recipeImage = document.querySelector('#recipe-image')
 const recipeSteps = document.querySelector('#recipe-steps')
-const cuisineList = document.querySelector('li')
+const cuisineListDiv = document.querySelector('#cuisine-list')
+
+function addCuisineToDivBar(recipe) {
+    const cuisineListElement = document.createElement('li')
+    cuisineListElement.textContent = recipe.cuisine
+    cuisineListDiv.appendChild(cuisineListElement)
+    cuisineListElement.addEventListener('mouseover', () => displayDetails(recipe))
+}
 
 function displayDetails(recipe) {
     recipeName.textContent = recipe.name
     recipeImage.src = recipe.image
-    recipeSteps.textContent = recipe.steps
-    // recipeSteps.forEach(recipe => addSteps(recipe))
-    cuisineList.addEventListener('click', () => displayDetails(recipe))
+    recipeSteps.forEach(recipe => addSteps(recipe))
+    cuisineListElement.addEventListener('click', () => displayDetails(recipe))
 }
 
-// function addSteps(recipe) {
-//     recipeSteps.textContent = recipe
-// }
+function addSteps(recipe) {
+    const ptag = document.createElement('p')
+    ptag.textontent = recipe
+    recipeSteps.textContent = recipe
+}
