ee1a928d5d490a1174e63f0154653cb3d8766bfd
Ashley Rivera <ashleyariv@gmail.com>
2023-08-17 14:58:54 -0400

    final project

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Top NYC Cuisines: Recipe Page
+# Top 5 NYC Cuisine Recipes
+Everyone knows that New York City has a huge food scene and some of the best food in the country. Having experienced this first hand, creating a web page of the top 5 cuisines in the city felt natural. On this page you will find the most classic dish for each of these 5 cusisines and a recipe for you to recreate these dishes right at home. 
+
+## Core Features 
+-> Hover over each cuisine name to display the ingredients for that dish along with a step by step list of how to make it, plus a picture as a reference for what your end result might look like. 
+
+-> Add comments to each dish as well as see previous comments from other viewers. Like or dislike comments, after one comment is disliked 10 times it will be deleted from the page for being unpopular. 
+
+-> Comments will persist between dishes and refreshes. 
+
+## Setup 
+-> Open the index.html in the terminal of the project by typing:
+
+open index.html
+
+-> Then run the the JSON server from the terminal/directory that the project is in by typing in: 
+
+json-server --watch db.json
+
+-> Finally, refresh the page where the project orinally opened in your browser to get the contents up and running. 
--- index.js
+++ index.js
@@ -1,28 +1,109 @@
-fetch('http://localhost:3000/recipes')
-.then(response => response.json())
-.then(recipes => recipes.forEach(recipe => addCuisineToDivBar(recipe)))
-
+const cuisineListDiv = document.querySelector('#cuisine-list')
 const recipeName = document.querySelector('#recipe-name')
 const recipeImage = document.querySelector('#recipe-image')
-const recipeSteps = document.querySelector('#recipe-steps')
-const cuisineListDiv = document.querySelector('#cuisine-list')
+const recipeStepsParentDiv = document.querySelector('#parent') 
+const stepsContainer = document.querySelector('#steps-container')
+const ingredientsContainer = document.querySelector('#ingredients-container')
+const commentSectionDiv = document.querySelector('#comments-section')
+const commentInput = document.querySelector('#comment-input')
+const formSubmission = document.querySelector('#comment-form')
+let currentRecipe
+
+fetch('http://localhost:3000/recipes')
+.then(response => response.json())
+.then(recipes => {
+    displayDetails(recipes[0])
+    recipes.forEach(recipe => {
+        addCuisineToDivBar(recipe)
+    })
+})
 
 function addCuisineToDivBar(recipe) {
     const cuisineListElement = document.createElement('li')
+    cuisineListElement.setAttribute('id', 'individual-cuisine')
     cuisineListElement.textContent = recipe.cuisine
     cuisineListDiv.appendChild(cuisineListElement)
-    cuisineListElement.addEventListener('mouseover', () => displayDetails(recipe))
+    cuisineListElement.addEventListener('mouseover', () => {
+        if (recipeStepsParentDiv.style.display === 'none') {
+            recipeStepsParentDiv.style.display = 'flex'
+        }
+        displayDetails(recipe)
+    })
 }
 
 function displayDetails(recipe) {
+
+    currentRecipe = recipe
+    
     recipeName.textContent = recipe.name
     recipeImage.src = recipe.image
-    recipeSteps.forEach(recipe => addSteps(recipe))
-    cuisineListElement.addEventListener('click', () => displayDetails(recipe))
+    ingredientsContainer.textContent = ""
+    recipe.ingredients.forEach(recipe => addIngredients(recipe))
+    stepsContainer.textContent = ""
+    recipe.steps.forEach(recipe => addSteps(recipe))
+    commentSectionDiv.textContent = ""
+    recipe.comments.forEach(comment => addComment(comment))
+}
+
+function addIngredients(recipe) {
+    const pTag = document.createElement('p')
+    pTag.textContent = recipe
+    ingredientsContainer.appendChild(pTag)
 }
 
 function addSteps(recipe) {
-    const ptag = document.createElement('p')
-    ptag.textontent = recipe
-    recipeSteps.textContent = recipe
+    const pTag = document.createElement('p')
+    pTag.textContent = recipe
+    stepsContainer.appendChild(pTag)
+}
+
+function addComment(comment) {
+    const newComment = document.createElement('p')
+    newComment.setAttribute('id', 'new-comment')
+    newComment.textContent = comment
+    commentSectionDiv.appendChild(newComment)
+
+    const likeButton = document.createElement('button')
+    likeButton.textContent = 0
+    const likeImage = document.createElement('img')
+    likeImage.setAttribute('id', 'like-image')
+    likeImage.src = 'https://www.hubpng.com/files/preview/800x800/png-transparent-thumbs-up-transparent-background-clip-art-thumbs-up-11681695792mrli0prk2gmmxukieqxaxg6rvsnstojugdd3xuy6nw2ukfalbth9buvdihfs3hxmwngh0cwydlcy8mmajkqyi.png'
+    likeButton.appendChild(likeImage)
+    const dislikeButton = document.createElement('button')
+    dislikeButton.textContent = 0 
+    const dislikeImage = document.createElement('img')
+    dislikeImage.setAttribute('id', 'dislike-image')
+    dislikeImage.src = 'https://www.pngfind.com/pngs/m/44-445048_thumbs-down-comments-thumbs-down-icon-png-transparent.png'
+    dislikeButton.appendChild(dislikeImage)
+    newComment.append(likeButton, dislikeButton)
+
+    likeButton.addEventListener('click', () => {
+        likeButton.textContent =  Number(likeButton.textContent) +1
+        likeButton.appendChild(likeImage)
+    })
+    dislikeButton.addEventListener('click', () => {
+        dislikeButton.textContent = Number(dislikeButton.textContent) +1
+        dislikeButton.appendChild(dislikeImage)
+        if (dislikeButton.textContent === '9') alert('Careful! One more dislike, and this comment will be deleted!') 
+        if (dislikeButton.textContent === '10') newComment.remove()
+    })
+}
+formSubmission.addEventListener('submit', (event) => {
+    event.preventDefault()
+    addComment(commentInput.value)
+    updateComments(currentRecipe)
+    event.target.reset()
+})
+
+function updateComments(recipe) {
+    fetch(`http://localhost:3000/recipes/${recipe.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        },
+        body: JSON.stringify({
+            comments: [...recipe.comments, commentInput.value]
+        })
+    })
 }
